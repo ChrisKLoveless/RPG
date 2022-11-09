f4fd186089284419823a7cf447e577881a9eed07
brandonf55 <Brandon.fenk99@gmail.com>
2022-11-09 11:55:05 -0800

    add passing test for monsterRegHit
    co-authored-by: Chris Loveless chriskloveless@gmail.com

--- __tests__/monster.test.js
+++ __tests__/monster.test.js
@@ -10,5 +10,14 @@ describe ("Monster", () => {
   test("this test should make a monster object", () => { 
     expect(newMonster.hp).toEqual(150); 
   });
+  
+  test("should update the monster hp" , () => {
+    newMonster.monsterCrit();
+    expect(newMonster.hp).toEqual(125);
+  });
 
+  test("should update the monsters hp", () =>{
+    newMonster.monsterRegHit();
+    expect(newMonster.hp).toEqual(140);
+  });
 });
--- src/js/monster.js
+++ src/js/monster.js
@@ -2,4 +2,14 @@ export default class Monster {
   constructor(hp) {
     this.hp = hp;
   }
+
+  monsterCrit() {
+    let critHit = 25;
+    this.hp -= critHit;
+  }
+
+  monsterRegHit(){
+    let regHit = 10;
+    this.hp -= regHit;
+  }
 }
